b84e6207d41da76a119c3a809ae87a5118e1666a
Sven Fischer <git-dev@linux4tw.de>
2021-11-10 00:34:31 +0100

    override mapFrameInitialized()

--- src/de/strubbl/java/changessum/ChangesSumPlugin.java
+++ src/de/strubbl/java/changessum/ChangesSumPlugin.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.openstreetmap.josm.data.APIDataSet;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 import org.openstreetmap.josm.tools.Logging;
@@ -18,12 +19,20 @@ public class ChangesSumPlugin extends Plugin {
 	public ChangesSumPlugin(PluginInformation info) {
 		super(info);
 		// init your plugin
-		Logging.debug("ChangesSumPlugin init");
+		Logging.debug("ChangesSumPlugin constructor");
+	}
+
+	public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+		if (oldFrame == null && newFrame != null) { // map frame added
+			Logging.debug("ChangesSumPlugin mapFrameInitialized: map frame added");
+		}
+		getObjectChangesSum();
 	}
 
 	private int getObjectChangesSum() {
 		APIDataSet apiData = new APIDataSet(MainApplication.getLayerManager().getEditDataSet());
 		List<OsmPrimitive> l = apiData.getPrimitives();
+		Logging.info("ChangesSumPlugin getObjectChangesSum: l.size()=" + l.size());
 		return l.size();
 	}
 }
