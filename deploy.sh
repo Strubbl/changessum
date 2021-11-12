b603ef44c9d86e6b50d689cd97c0eac6844387f1
Sven Fischer <git-dev@linux4tw.de>
2021-11-12 01:25:33 +0100

    use test josm home for plugin dev

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eux
-cp ../../dist/changessum.jar ~/.local/share/JOSM/plugins/
+#cp ../../dist/changessum.jar ~/.local/share/JOSM/plugins/
+cp ../../dist/changessum.jar ~/tmp/josm-dev-home/plugins/
 cp ../../dist/changessum.jar ~/git/changessum/dist/
 cp -r * ~/git/changessum/
